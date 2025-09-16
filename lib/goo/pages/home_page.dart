9c11208da4e46b8c9118b66ee74c96b3416124bd
Yuri Trindade <yuritrindade.play55@gmail.com>
2025-09-16 16:26:32 -0300

    recommended sections completed

--- lib/goo/pages/home_page.dart
+++ lib/goo/pages/home_page.dart
@@ -42,6 +42,8 @@ class _HomePageState extends State<HomePage> {
             final cities = snapshot.data!;
 
             final popularCities = cities.where((c) => c.rating > 4.7).toList();
+            final recommendedCities =
+                cities.where((c) => c.rating <= 4.7).toList();
 
             return SingleChildScrollView(
               child: Column(
@@ -52,41 +54,11 @@ class _HomePageState extends State<HomePage> {
                   const SizedBox(height: 28),
 
                   // Search
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: Container(
-                      padding: const EdgeInsets.all(16),
-                      height: 60,
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                        color: Colors.grey[200],
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          const Icon(
-                            Icons.search,
-                            size: 30,
-                            color: Colors.grey,
-                          ),
-                          const SizedBox(width: 6),
-                          const Expanded(
-                            child: TextField(
-                              textAlign: TextAlign.left,
-                              decoration: InputDecoration(
-                                hintText: 'Find things to do',
-                                border: InputBorder.none,
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
+                  HomeSearchWidget(),
 
-                  // Popular cities
+                  // Popular cities section
                   Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Row(
                         children: const [
@@ -98,7 +70,7 @@ class _HomePageState extends State<HomePage> {
                             child: Text(
                               'Popular',
                               style: TextStyle(
-                                fontSize: 16,
+                                fontSize: 18,
                                 color: Colors.black,
                                 fontWeight: FontWeight.w700,
                               ),
@@ -116,7 +88,7 @@ class _HomePageState extends State<HomePage> {
                               padding:
                                   const EdgeInsets.symmetric(horizontal: 15),
                               child: Container(
-                                margin: EdgeInsets.symmetric(vertical: 20),
+                                margin: EdgeInsets.symmetric(vertical: 16),
                                 width: 220,
                                 height: 240,
                                 decoration: BoxDecoration(
@@ -126,7 +98,7 @@ class _HomePageState extends State<HomePage> {
                                       BoxShadow(
                                         color:
                                             Colors.black.withValues(alpha: 0.3),
-                                        blurRadius: 20,
+                                        blurRadius: 16,
                                         offset: const Offset(-2, 2),
                                       ),
                                     ]),
@@ -213,12 +185,179 @@ class _HomePageState extends State<HomePage> {
                           },
                         ),
                       ),
+                      Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                        child: Text(
+                          'Recommended',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(
+                            bottom: 22, left: 16, right: 16, top: 8),
+                        child: GridView.count(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: 12,
+                          mainAxisSpacing: 12,
+                          childAspectRatio: 1.3,
+                          shrinkWrap: true,
+                          physics: NeverScrollableScrollPhysics(),
+                          children: List.generate(
+                            recommendedCities.length,
+                            (index) => Container(
+                              height: 1,
+                              width: 100,
+                              decoration: BoxDecoration(
+                                  color: Colors.white.withValues(alpha: .7),
+                                  borderRadius: BorderRadius.circular(25),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color:
+                                          Colors.black.withValues(alpha: 0.3),
+                                      blurRadius: 16,
+                                      offset: const Offset(-2, 2),
+                                    ),
+                                  ]),
+                              child: Stack(
+                                children: [
+                                  Column(
+                                    children: [
+                                      SizedBox(
+                                        height: 100,
+                                        width: double.infinity,
+                                        child: ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(25),
+                                          child: Image.asset(
+                                            recommendedCities[index].assetPath,
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  Padding(
+                                    padding:
+                                        EdgeInsets.only(left: 16, bottom: 8),
+                                    child: Align(
+                                      alignment: Alignment.bottomLeft,
+                                      child: Text(
+                                        recommendedCities[index].name,
+                                        style: TextStyle(
+                                          fontSize: 14,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w700,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Align(
+                                    alignment: Alignment.bottomRight,
+                                    child: Padding(
+                                      padding: EdgeInsets.only(
+                                          bottom: 20, right: 15),
+                                      child: Container(
+                                        padding: EdgeInsets.all(3),
+                                        height: 30,
+                                        width: 50,
+                                        decoration: BoxDecoration(
+                                            color: Colors.white
+                                                .withValues(alpha: .6),
+                                            borderRadius:
+                                                BorderRadius.circular(25)),
+                                        child: Container(
+                                          height: double.infinity,
+                                          width: double.infinity,
+                                          decoration: BoxDecoration(
+                                            color: Colors.grey.shade700,
+                                            borderRadius:
+                                                BorderRadius.circular(25),
+                                          ),
+                                          child: Center(
+                                            child: Row(
+                                                mainAxisSize: MainAxisSize.min,
+                                                children: [
+                                                  Icon(
+                                                    Icons.star,
+                                                    color: Colors.yellow,
+                                                    size: 14,
+                                                  ),
+                                                  Text(
+                                                    recommendedCities[index]
+                                                        .rating
+                                                        .toString(),
+                                                    style: TextStyle(
+                                                      fontSize: 12,
+                                                      color: Colors.white,
+                                                      fontWeight:
+                                                          FontWeight.w500,
+                                                    ),
+                                                  ),
+                                                ]),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
                     ],
                   ),
                 ],
               ),
             );
           },
+        ),
+      ),
+    );
+  }
+}
+
+class HomeSearchWidget extends StatelessWidget {
+  const HomeSearchWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        height: 60,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            const Icon(
+              Icons.search,
+              size: 30,
+              color: Colors.grey,
+            ),
+            const SizedBox(width: 6),
+            const Expanded(
+              child: TextField(
+                textAlign: TextAlign.left,
+                decoration: InputDecoration(
+                  hintText: 'Find things to do',
+                  border: InputBorder.none,
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
---
+++ lib/goo/widgets/home_search_widget.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class HomeSearchWidget extends StatelessWidget {
+  const HomeSearchWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        height: 60,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            const Icon(
+              Icons.search,
+              size: 30,
+              color: Colors.grey,
+            ),
+            const SizedBox(width: 6),
+            const Expanded(
+              child: TextField(
+                textAlign: TextAlign.left,
+                decoration: InputDecoration(
+                  hintText: 'Find things to do',
+                  border: InputBorder.none,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
